b8a503a1f2dfb9117d4cacda74c7873b19141889
Orlando Ortega Jr <orlandoortegajr@gmail.com>
2021-05-29 12:23:41 -0400

    setup express init

---
+++ server/app.js
@@ -0,0 +1,12 @@
+const e = require('express');
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
